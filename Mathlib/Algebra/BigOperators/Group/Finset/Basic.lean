4c30fc0e95074650478d65778968d57c95897787
Christopher J. R. Lloyd <cjl8zf@virginia.edu>
2026-03-19 19:43:03 -0400

    Remove checks

--- Mathlib/Algebra/BigOperators/Group/Finset/Basic.lean
+++ Mathlib/Algebra/BigOperators/Group/Finset/Basic.lean
@@ -985,10 +985,6 @@ theorem card_eq_sum_card_image [DecidableEq M] (f : ι → M) (s : Finset ι) :
     #s = ∑ b ∈ s.image f, #{a ∈ s | f a = b} :=
   card_eq_sum_card_fiberwise fun _ => mem_image_of_mem _
 
-#check Finset.sdiff_eq_self_of_disjoint
-#check Finset.disjoint_right
-#check Finset.compl_eq_univ_sdiff
-
 /-- Given a finite collection of finite subsets $B_1, \ldots, B_k$ and, for every
 $x \in \bigcup_i B_i$, let $C_x$ be the set of indices of the $B_i$'s that contain $x$.
 Then, $\sum_i |B_i| = \sum_x |C_x|$. -/
